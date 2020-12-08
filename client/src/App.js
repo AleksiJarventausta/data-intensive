157205f0f64eaff9f95f0845ae29261c32f972ad
Petteri Mäkelä <p-mak@pop-os.localdomain>
2020-12-08 17:11:51 +0200

    semanticUI + init

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Button, Grid, Header} from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Grid textAlign='center' verticalAlign='middle' style={{ height:'100vh'}}>
+        <Switch>
+          <Route exact path="/">
+            <Grid.Column style={{maxWidth:450}}>
+              <Header as='h2'>  aaa </Header>
+                <Button as={Link} to="/main"> Login Button</Button>
+            </Grid.Column>
+          </Route>
+            <Route path="/main">
+              <Grid.Column style={{maxWidth:450}}>
+                <Header as='h2'>  eee </Header>
+                  <Button as={Link} to="/"> Log Out Button</Button>
+              </Grid.Column>
+            </Route>
+          </Switch>
+      </Grid>
+    </Router>
   );
 }
 
