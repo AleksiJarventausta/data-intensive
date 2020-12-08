169223d93065fc27b109ab35ab3749169eea0abd
aleksi <aksu.j@hotmail.com>
2020-12-08 15:19:41 +0200

    init.

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
---
+++ server/validation/validateRegistration.js
@@ -0,0 +1,30 @@
+const Validator = require("validator");
+var isEmpty = require("is-empty");
+
+module.exports = function validateRegisteration(data) {
+  let errors = {};
+
+  //convert empty points to strings so we can use validator
+  data.username = !isEmpty(data.username) ? data.username : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.passwordconfirm = !isEmpty(data.passwordconfirm)
+    ? data.passwordconfirm
+    : "";
+
+  if (Validator.isEmpty(data.username)) {
+    errors.username = "Name field can't be empty.";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field can't be empty.";
+  } else if (!Validator.isLength(data.password, { min: 8, max: 32 })) {
+    errors.password = "Password has to be 8 to 32 characters long.";
+  } else if (data.password !== data.passwordconfirm) {
+    errors.passwordconfirm = "Didn't match the first one.";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
