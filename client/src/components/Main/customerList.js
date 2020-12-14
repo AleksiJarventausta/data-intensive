4bf49ce190b98a259c9b05fefdfd07e150ca5a6a
Jussi Moilanen <Juissiboy123@gmail.com>
2020-12-14 22:05:03 +0200

    init UI

--- client/src/components/Main/customerList.js
+++ client/src/components/Main/customerList.js
@@ -1,34 +1,43 @@
-import { Button, Grid, Header, Form, List } from "semantic-ui-react";
-import { Redirect } from "react-router-dom";
-import NewForm from "./NewUserForm";
+import { List, Button, Image } from "semantic-ui-react";
 import { useEffect, useState } from "react";
 
-//tällä hetkellä toimisi jos saisi listan eikä objectin
-
 function CustomerListing() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/customer")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
-  }, []);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8080/customer");
+    const data = await response.json();
+    setItems(data);
+  };
 
   return (
     <>
-      <h1>Customers</h1>
-      <List>
-        {items.map((item) => (
-          <List.Item key={item._id}>
-            <List.Content> {item.firstname}</List.Content>
-            <List.Content> {item.lastname}</List.Content>
-            <List.Content> {item.address}</List.Content>
-            <List.Content> {item.ssn}</List.Content>
-          </List.Item>
-        ))}
-      </List>
+      <h3>Customers</h3>
+      {items.length > 0 ? (
+        <List>
+          {items.map((item) => (
+            <List.Item key={item._id}>
+              <List.Content>
+                <List.Header as="a">
+                  {"Name: "}
+                  {item.firstname} {item.lastname}
+                </List.Header>
+              </List.Content>
+              <List.Icon name="mail" />
+              <List.Content>
+                {" "}
+                {"Address: "} {item.address}
+              </List.Content>
+              <List.Content>
+                {" "}
+                {"Social security: "} {item.ssn}
+              </List.Content>
+            </List.Item>
+          ))}
+        </List>
+      ) : (
+        <Button onClick={fetchData}>Fetch customers</Button>
+      )}
     </>
   );
 }
