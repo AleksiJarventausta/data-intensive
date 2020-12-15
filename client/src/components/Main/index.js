926d2c952a567f2331a9d46d5b79f1730d49588f
Jussi Moilanen <Juissiboy123@gmail.com>
2020-12-15 18:17:29 +0200

    conditional render

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,14 +1,13 @@
 import { Container, Grid, Header, List, Button } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
-import {useState} from "react";
+import { useState } from "react";
 import NewForm from "./NewUserForm";
 import CustomerList from "./customerList";
 import CornerStoneViewPort from "./CornerstoneViewport";
 import CustomMenu from "./CustomMenu";
 
-
 function Main(props) {
-  const [customerId, setCustomerId] = useState("5fd5ffc5d621e849cc427a00");
+  const [customerId, setCustomerId] = useState("");
   const handleLogout = () => {
     props.onLogout({}, "/");
   };
@@ -17,34 +16,39 @@ function Main(props) {
     return <Redirect to={props.tab} />;
   }
 
+  const isOptician = () => {
+    return props.user.profession === 1 ? true : false;
+  };
+  const isOpth = () => {
+    return props.user.profession === 2 ? true : false;
+  };
+
   return (
-
-
-      <Grid container columns={2} relaxed="very">
-        <CustomMenu username={props.user.username} onLogout={handleLogout}/>
-        <Grid.Row>
-
-        </Grid.Row>
-        <Grid.Row>
+    <Grid container columns={2} relaxed="very">
+      <CustomMenu username={props.user.username} onLogout={handleLogout} />
+      <Grid.Row> </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <CustomerList setCustomerId={setCustomerId} />
+        </Grid.Column>
+        {isOptician() ? (
           <Grid.Column width={8}>
             <NewForm />
           </Grid.Column>
-          <Grid.Column width={8}>
-            <CustomerList setCustomerId={setCustomerId} />
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <CornerStoneViewPort customerId={customerId} />
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <h1>Lisää </h1>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
-
+        ) : (
+          <div></div>
+        )}
+      </Grid.Row>
+      {customerId.length > 0 && isOpth() ? (
+        <CornerStoneViewPort customerId={customerId} />
+      ) : (
+        <div></div>
+      )}
+      <Grid.Row>
+        <Grid.Column width={8}></Grid.Column>
+        <Grid.Column width={8}></Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
 
