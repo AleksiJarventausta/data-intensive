0dddee26dbc696522f2ad8b87fb04c697be37951
Jussi Moilanen <Juissiboy123@gmail.com>
2020-12-15 19:04:09 +0200

    update cert route

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -2,30 +2,14 @@ const router = require("express").Router();
 const Customer = require("../connections/optician_1").model("Customer");
 const mongoose = require("mongoose");
 const Image = require("../connections/optician_1").model("Image");
-const passport = require("passport");
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
-
-const opth1 = require("../connections/opthal_1");
-const opth2 = require("../connections/opthal_2");
-const optician = require("../connections/optician_1");
-
-const opthMap = new Map();
-opthMap.set("opthal_1", opth1);
-opthMap.set("opthal_2", opth2);
-const allMap = new Map();
-allMap.set("opthal_1", opth1);
-allMap.set("opthal_2", opth2);
-allMap.set("optician_1", opth2);
-
-
 
 router.put(
   "/newcustomerwithimage",
   upload.single("image"),
   function (req, res) {
-    let opth_db_key = getRandomKey(opthMap);
     let customerData = {
       _id: new mongoose.Types.ObjectId(),
       firstname: req.body.firstname,
@@ -33,7 +17,6 @@ router.put(
       address: req.body.address,
       ssn: req.body.SSN,
       certification_status: false,
-      ophthalmologist_db: opth_db_key,
     };
     if (req.file) {
       customerData.image = req.file.buffer;
@@ -43,15 +26,7 @@ router.put(
     newCustomer
       .save()
       .then((item) => {
-        let ophth_db = opthMap.get(opth_db_key);
-        let OpthCustomer = ophth_db.model("Customer");
-        let opthCustomer = new OpthCustomer( customerData );
-        opthCustomer
-          .save()
-          .then(() => res.send("ok"))
-          .catch((err) => {res.status(400).send("unable to save to database")
-          console.log(err);
-        });
+        res.send("ok");
       })
       .catch((err) => {
         console.log(err);
@@ -59,6 +34,21 @@ router.put(
       });
   }
 );
+
+router.post("/updatecertification", function (req, res) {
+  Customer.findByIdAndUpdate(
+    req.body._id,
+    { certification_status: req.body.certification_status },
+    function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(data);
+        console.log("Data updated!");
+      }
+    }
+  );
+});
 
 router.post("/newcustomer", function (req, res) {
   let newCustomer = new Customer({
@@ -82,28 +72,15 @@ router.post("/newcustomer", function (req, res) {
 });
 
 // 8080/Customer
-router.get(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  function (req, res) {
-    allMap.get(req.user.own_db).model("Customer").find()
-      .select("-image")
-      .exec(function (err, customers) {
-        if (err) return res.status(400).json({ error: "failed" });
-        if (customers) {
-          res.send(customers);
-        }
-      });
-  }
-);
-function getRandomKey(collection) {
-  let index = Math.floor(Math.random() * collection.size);
-  let cntr = 0;
-  for (let key of collection.keys()) {
-    if (cntr++ === index) {
-      return key;
-    }
-  }
-}
+router.get("/", function (req, res) {
+  Customer.find()
+    .select("-image")
+    .exec(function (err, customers) {
+      if (err) return res.status(400).json({ error: "failed" });
+      if (customers) {
+        res.send(customers);
+      }
+    });
+});
 
 module.exports = router;
